b54b3176b813997030e4ec084d7e323371fa5d74
Dekel Naar <dekel.naar@onavo.com>
2013-07-18 00:51:44 +0300

    added gradients and colors

--- src/com/dekel/babysitter/MainActivity.java
+++ src/com/dekel/babysitter/MainActivity.java
@@ -11,6 +11,14 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private boolean isFirstTime = true; // TODO repo
+    private enum State {
+        SHOWING_TOS,
+        TOS_APPROVED_SHOWING_INTRO,
+    }
+
+    private State state;
+
 
     /**
      * Called when the activity is first created.
@@ -18,7 +26,53 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        if (isFirstTime) {
+            setContentView(R.layout.terms_of_service);
+
+            state = State.SHOWING_TOS;
+
+            Typeface typeFace = Typeface.createFromAsset(getAssets(),"Alef-Regular.ttf");
+            Typeface typeFaceBold = Typeface.createFromAsset(getAssets(),"Alef-Bold.ttf");
+            ((TextView) findViewById(R.id.mainTitleTextView)).setTypeface(typeFace);
+
+            final TextView bodyView = (TextView) findViewById(R.id.mainBodyTextView1);
+            bodyView.setTypeface(typeFace);
+            final TextView subtitleView = (TextView) findViewById(R.id.mainSubtitleTextView);
+            subtitleView.setTypeface(typeFaceBold);
+
+            final Button continueButton = (Button) findViewById(R.id.button);
+            continueButton.setTypeface(typeFace);
+            continueButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    switch (state) {
+                        case SHOWING_TOS:
+                            state = State.TOS_APPROVED_SHOWING_INTRO;
+                            // Live switching
+                            subtitleView.setText("על נוסעים לגן");
+                            bodyView.setText("אפליקציית \"נוסעים לגן\" מבצעת מעקב אחרי נסיעותיכם ברכב ותזכיר לכם לוודא שלא שכחתם את ילדכם ברכב זמן קצר לאחר יציאתכם מהרכב.\n");
+                            continueButton.setText("המשך");
+
+                            break;
+
+                        case TOS_APPROVED_SHOWING_INTRO:
+                            subtitleView.setText("ניצחון!!!!!");
+                            break;
+                        default:
+                            break;
+                    }
+
+
+
+
+
+                }
+            });
+
+
+
+        }
+
 
         // TODO alarm service for BT
         // TODO when ride in progress - present it here - singleton?
@@ -37,12 +91,7 @@ public class MainActivity extends Activity {
 
         Log.d(Config.MODULE_NAME, "Init done!");
 
-        Typeface typeFace = Typeface.createFromAsset(getAssets(),"Alef-Regular.ttf");
-        Typeface typeFaceBold = Typeface.createFromAsset(getAssets(),"Alef-Bold.ttf");
-        ((TextView) findViewById(R.id.mainTitleTextView)).setTypeface(typeFace);
-        ((TextView) findViewById(R.id.mainSubtitleTextView)).setTypeface(typeFaceBold);
-        ((TextView) findViewById(R.id.mainBodyTextView1)).setTypeface(typeFace);
-        ((Button) findViewById(R.id.button)).setTypeface(typeFace);
+
 
     }
 
