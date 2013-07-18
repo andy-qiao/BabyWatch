cc31499ba77d0af70638f846e1a5eca43224b339
Dekel Naar <dekel.naar@onavo.com>
2013-07-18 20:41:44 +0300

    refactored a bit, now cleaner

--- src/com/dekel/babysitter/Config.java
+++ src/com/dekel/babysitter/Config.java
@@ -8,11 +8,9 @@ package com.dekel.babysitter;
  * To change this template use File | Settings | File Templates.
  */
 public class Config {
+    public static final String FIRST_RUN_COMPLETED_KEY = "first_run";
+    public static final String MODULE_MAIN_PREFERENCE = "prefs";
     public static String MODULE_NAME = "babysitter_log";
 
     public static float SPEED_THRESHOLD = 8; // ~25Km/h / 3.6m/s
-
-    public static boolean debug = true;
-
-    public static String PREFS_NAME = "prefs";
 }
--- src/com/dekel/babysitter/MainActivity.java
+++ src/com/dekel/babysitter/MainActivity.java
@@ -13,23 +13,15 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    public static final String MODULE_MAIN_PREFERENCE = "bla";
-    public static final String FIRST_RUN_COMPLETED_KEY = "first_run";
-
-    private boolean isRideInProgress = false; // TOOD state machine.
-    private boolean demoError = true;
-    private boolean demoAlert = false;
-
-
-    private enum State {
+    private enum SliderState {
         SHOWING_TOS,
         TOS_APPROVED_SHOWING_INTRO,
     }
 
-    private State state = State.SHOWING_TOS;
-
-
-
+    private boolean isRideInProgress = false; // TODO StateMachine.
+    private boolean demoError = true;
+    private boolean demoAlert = false;
+    private SliderState sliderState = SliderState.SHOWING_TOS;
 
     /**
      * Called when the activity is first created.
@@ -38,50 +30,23 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (demoAlert) {
+        if (demoAlert) { // TODO debug remove me
             startActivity(new Intent(this, AlertActivity.class));
         }
 
         if (!isFirstTimeCompleted()) { // First time!
-            setContentView(R.layout.terms_of_service);
+            showFirstTimeSlider();
 
-            state = State.SHOWING_TOS;
-
-            Typeface typeFace = Typeface.createFromAsset(getAssets(),"Alef-Regular.ttf");
-            Typeface typeFaceBold = Typeface.createFromAsset(getAssets(),"Alef-Bold.ttf");
-            ((TextView) findViewById(R.id.mainTitleTextView)).setTypeface(typeFace);
-
-            final TextView bodyView = (TextView) findViewById(R.id.mainBodyTextView1);
-            bodyView.setTypeface(typeFace);
-            final TextView subtitleView = (TextView) findViewById(R.id.mainSubtitleTextView);
-            subtitleView.setTypeface(typeFaceBold);
-
-            final Button continueButton = (Button) findViewById(R.id.button);
-            continueButton.setTypeface(typeFace);
-            continueButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                switch (state) {
-                    case SHOWING_TOS:
-                        state = State.TOS_APPROVED_SHOWING_INTRO;
-                        // Live switching
-                        subtitleView.setText("על \"נוסעים לגן\"");
-                        bodyView.setText("אפליקציית \"נוסעים לגן\" מבצעת מעקב אחרי נסיעותיכם ברכב. זמן קצר לאחר סיום הנסיעה המערכת תזכיר לכם לוודא שלא שכחתם את ילדכם\n");
-                        continueButton.setText("המשך");
-                        break;
-
-                    case TOS_APPROVED_SHOWING_INTRO:
-                        setFirstTimeCompleted();
-                        showSystemReady();
-                        break;
-                }
-                }
-            });
-
-        } else { // Not first time!
+        } else {
             showSystemReady();
         }
 
+        InitializeMonitoring();
 
+        Log.d(Config.MODULE_NAME, "Init done!");
+    }
+
+    private void InitializeMonitoring() {
         // TODO alarm service for BT
         // TODO when ride in progress - present it here - singleton?
 
@@ -96,20 +61,51 @@ public class MainActivity extends Activity {
 //                pi);
 
         startService(new Intent(this, BabyMonitorService.class));
+    }
 
-        Log.d(Config.MODULE_NAME, "Init done!");
+    private void showFirstTimeSlider() {
+        setContentView(R.layout.terms_of_service);
 
+        sliderState = SliderState.SHOWING_TOS;
 
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"Alef-Regular.ttf");
+        Typeface typeFaceBold = Typeface.createFromAsset(getAssets(),"Alef-Bold.ttf");
+        ((TextView) findViewById(R.id.mainTitleTextView)).setTypeface(typeFace);
 
+        final TextView bodyView = (TextView) findViewById(R.id.mainBodyTextView1);
+        bodyView.setTypeface(typeFace);
+        final TextView subtitleView = (TextView) findViewById(R.id.mainSubtitleTextView);
+        subtitleView.setTypeface(typeFaceBold);
+
+        final Button continueButton = (Button) findViewById(R.id.button);
+        continueButton.setTypeface(typeFace);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            switch (sliderState) {
+                case SHOWING_TOS:
+                    sliderState = SliderState.TOS_APPROVED_SHOWING_INTRO;
+                    // Live switching
+                    subtitleView.setText("על \"נוסעים לגן\"");
+                    bodyView.setText("אפליקציית \"נוסעים לגן\" מבצעת מעקב אחרי נסיעותיכם ברכב. זמן קצר לאחר סיום הנסיעה המערכת תזכיר לכם לוודא שלא שכחתם את ילדכם\n");
+                    continueButton.setText("המשך");
+                    break;
+
+                case TOS_APPROVED_SHOWING_INTRO:
+                    setFirstTimeCompleted();
+                    showSystemReady();
+                    break;
+            }
+            }
+        });
     }
 
     private boolean isFirstTimeCompleted() {
-        return getSharedPreferences(MODULE_MAIN_PREFERENCE, MODE_PRIVATE).getBoolean(FIRST_RUN_COMPLETED_KEY, false);
+        return getSharedPreferences(Config.MODULE_MAIN_PREFERENCE, MODE_PRIVATE).getBoolean(Config.FIRST_RUN_COMPLETED_KEY, false);
     }
 
     private void setFirstTimeCompleted() {
-        SharedPreferences.Editor e = getSharedPreferences(MODULE_MAIN_PREFERENCE, MODE_PRIVATE).edit();
-        e.putBoolean(FIRST_RUN_COMPLETED_KEY, true);
+        SharedPreferences.Editor e = getSharedPreferences(Config.MODULE_MAIN_PREFERENCE, MODE_PRIVATE).edit();
+        e.putBoolean(Config.FIRST_RUN_COMPLETED_KEY, true);
         e.commit();
     }
 
